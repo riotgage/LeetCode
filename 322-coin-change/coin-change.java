cccf3e2f8421055dd2d1ac0a20315678d388713e
Rohit Satwadhar <rvsatwadhar@gmail.com>
2024-10-13 16:01:00 +0530

    Time: 19 ms (40.74%) | Memory: 44.8 MB (32.73%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,13 +2,27 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length; 
         int dp[][] = new int [n+1][amount+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
         
-        int result = minCoinsChange(coins,amount,n,dp);;  
+        Arrays.fill(dp[0],Integer.MAX_VALUE-1);
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        
 
-        if(result == Integer.MAX_VALUE)return -1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
 
-        return result;
+                // If we include the current coin (coins[i-1]) and it's less than or equal to the amount
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i - 1]] + 1);
+                }
+            }
+        }
+
+        // If the value in dp[n][amount] is still set to the large value, it means it's not possible
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
     }
 
     public int minCoinsChange(int[] coins, int amount,int coinIndex,int dp[][]){
