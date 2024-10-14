ccba51894687e3cc205e8d6f4460b023c133c9af
Rohit Satwadhar <rvsatwadhar@gmail.com>
2024-10-14 12:54:10 +0530

    Time: 20 ms (77.42%) | Memory: 50.6 MB (78.07%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestCommonSubsequence(String s, String t) {
+        return lengthOfLcs(s,t,s.length(),t.length());
+    }
+
+    public  int lengthOfLcs(String s, String t,int n,int m){
+		if(n==0 || m==0)return 0;
+
+		int[][] dp =new int[n+1][m+1];
+
+		Arrays.fill(dp[0],0);
+
+		for(int i=0;i<=n;i++){
+			dp[i][0]=0;
+		}
+
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=m;j++){
+				if(s.charAt(i-1)==t.charAt(j-1)){
+					dp[i][j]=1+dp[i-1][j-1];
+				}else{
+					dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+				}
+			}
+		}
+
+		return dp[n][m];
+	}
+}
