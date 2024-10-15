1fe175070f29ee5f17b06928cd082aaf4a26931b
Rohit Satwadhar <rvsatwadhar@gmail.com>
2024-10-15 20:52:34 +0530

    Time: 8 ms (5.29%) | Memory: 42.6 MB (5.15%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sPointer = 0;
-        int tPointer = 0;
+        // int sPointer = 0;
+        // int tPointer = 0;
 
-        while(sPointer<s.length() && tPointer<t.length()){
-            if(s.charAt(sPointer)==t.charAt(tPointer)){
-                sPointer++;
-            }
-            tPointer++;
+        // while(sPointer<s.length() && tPointer<t.length()){
+        //     if(s.charAt(sPointer)==t.charAt(tPointer)){
+        //         sPointer++;
+        //     }
+        //     tPointer++;
+        // }
+
+        // if(sPointer==s.length())return true;
+
+        // return false;
+        int m = t.length();
+        int n = s.length();
+        
+        int dp[] [] = new int[n+1][m+1];
+
+        Arrays.fill(dp[0],0);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
 
-        if(sPointer==s.length())return true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
 
-        return false;
+        return dp[n][m] == n;
     }
 }
