ac9e0444d4242de69fa2f6aeae5804d30f8c8fbc
Rohit Satwadhar <rvsatwadhar@gmail.com>
2024-11-14 15:20:14 +0530

    Time: 66 ms (14.98%) | Memory: 44.9 MB (91.78%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -11,22 +11,6 @@ class Solution {
         for(int card:hand){
             counts.put(card,counts.getOrDefault(card,0)+1);
         }
-        // for(int card:counts.keySet()){
-        //     int freq = counts.get(card);
-
-        //     if(freq>0){
-
-        //         for(int i=0;i<groupSize;i++){
-        //             int currentCard = card+i;
-
-        //             if(counts.getOrDefault(currentCard,0)<=0){
-        //                 System.out.println(currentCard);
-        //                 return false;
-        //             }
-        //             counts.put(currentCard,counts.get(currentCard)-1);
-        //         }
-        //     }
-        // }
 
         while (!counts.isEmpty()) {
             Map.Entry<Integer, Integer> firstEntry = counts.firstEntry();
@@ -40,7 +24,7 @@ class Solution {
                 }
                 counts.put(currentCard, counts.get(currentCard) - 1);
                 if (counts.get(currentCard) == 0) {
-                    counts.remove(currentCard); // Remove the card if its count goes to zero
+                    counts.remove(currentCard);
                 }
             }
         }       
