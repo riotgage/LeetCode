dc2a7cbd0c48181f49859da128b2131141469ec1
Rohit Satwadhar <rvsatwadhar@gmail.com>
2024-12-16 22:17:01 +0530

    Time: 32 ms (21.07%) | Memory: 45.5 MB (25.51%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int len = nums.length;
+        if(len<2)return 0;
+        int[] minJumps = new int[len];
+
+        for(int i=0;i<len;i++){
+            minJumps[i]=Integer.MAX_VALUE;
+        }
+        minJumps[0]=0;
+
+        for(int i=0;i<len;i++){
+            int jump = nums[i];
+            for(int j=1;j<=jump && i+j<len;j++){
+                minJumps[i+j]=Math.min(minJumps[i+j],minJumps[i]+1);              
+            }
+
+            if(minJumps[len-1]!=Integer.MAX_VALUE)return minJumps[len-1];
+        }
+        return minJumps[len-1];
+    }
+}
