7cfb7d6a3c3cffcb1a9eecd23f2b42c799ab4369
Rohit Satwadhar <rvsatwadhar@gmail.com>
2025-02-09 15:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (96.39%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -15,15 +15,16 @@ class Solution {
 
             return;
         }
+        // Ignoring the first element in array
         getSubsets(nums,ans,index+1,partAns);
 
+        // Add the element and generate all the subsets which will have this one elem
         partAns.add(nums[index]);
         
         getSubsets(nums,ans,index+1,partAns);
 
+        // We generated all subsets with currrent elem remove it and generate subsets without it
         partAns.remove(partAns.size()-1);
-
-
 
     }
 }
