90023f395865c6e5a6e4fa87506e7d10b8a28fe2
Rohit Satwadhar <rvsatwadhar@gmail.com>
2025-06-20 15:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (73.61%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.java
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.java
@@ -4,9 +4,8 @@ class Solution {
         char lastChar = 'a';
         int secs = 0;
         for(char c: word.toCharArray()){
-            int moves = Math.min(Math.abs(c-lastChar),26-Math.abs(lastChar-c))+1;
-            System.out.println(moves);
-            secs+=moves;
+            int move = Math.abs(c-lastChar);
+            secs+=Math.min(move,26-move)+1;
             lastChar = c;
         }
         return secs;
