bc5cb18b87cc76f1ca4b3851859c9f4329b69b1b
Rohit Satwadhar <rvsatwadhar@gmail.com>
2025-09-20 23:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (67.20%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,22 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int onePointer=0;
-       int zeroPointer=0;
-       int twoPointer=nums.length-1;
+       int redPointer = 0;
+       int whitePointer = 0;
+       int bluePointer = nums.length-1;
 
-       while(onePointer<=twoPointer){
-            int num = nums[onePointer];
+       while(whitePointer<=bluePointer){
+            int flagColor=nums[whitePointer];
 
-            // swap with zero pointer and move ahead
-            if(num==0){
-                swap(nums,zeroPointer++,onePointer++);
-            }else if(num==1){
-                onePointer++;
+            if(flagColor==0){
+                swap(nums,redPointer++,whitePointer++);
+            }else if(flagColor==1){
+                whitePointer++;
             }else{
-                swap(nums,onePointer,twoPointer--);
+                swap(nums,whitePointer,bluePointer--);
             }
        }
-       
     }
 
     public void swap(int nums[],int i,int j){
