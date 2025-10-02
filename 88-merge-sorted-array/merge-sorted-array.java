4b8b9a8dce6e5f4d26a986442c1b635348568a29
Rohit Satwadhar <rvsatwadhar@gmail.com>
2025-10-02 16:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (8.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,5 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        // Start merging from the back.
+        // compare the biggest elements first
+
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
